077448b273708dd02836d12b90da3769018848f8
qi116 <qi116@purdue.edu>
2022-04-01 15:28:31 -0700

    Set up post request still needs testing

--- Farmers Market/Farmers Market/APIGet.swift
+++ Farmers Market/Farmers Market/APIGet.swift
@@ -19,13 +19,22 @@ class APIGet {
         let url = URL(string: "http://128.211.194.217:3000/api/hello")!
         var request = URLRequest(url:url)
         
-        request.httpMethod = "GET"
+        let json: [String: Any] = ["message": "hello"]
+        let jsonData = try? JSONSerialization.data(withJSONObject: json)
+        
+        
+        request.httpMethod = "POST"
+        request.httpBody = jsonData
         
         let task = URLSession.shared.dataTask(with: url) { data, response, error in
             
             if let data = data {
-                let image = UIImage(data: data)
-                print(response)
+                //let image = UIImage(data: data)
+                //print(response)
+                let jsonData = try? JSONSerialization.jsonObject(with: data, options: [])
+                if let jsonData = jsonData as? [String: Any] {
+                    print(jsonData)
+                }
             } else if let error = error {
                 print("Error \(error)")
             }

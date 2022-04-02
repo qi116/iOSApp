e7901645e98ad1213f2028d1237947755ed14cbb
qi116 <qi116@purdue.edu>
2022-04-01 18:57:49 -0700

    post request works

--- Farmers Market/Farmers Market/APIGet.swift
+++ Farmers Market/Farmers Market/APIGet.swift
@@ -19,9 +19,9 @@ class APIGet {
         let url = URL(string: "http://128.211.194.217:3000/api/user/login")!
         var request = URLRequest(url:url)
         
-        let json: [String: Any] = ["email": "test", "password": "test"]
-        let jsonData = try? JSONSerialization.data(withJSONObject: json)
-        //print(jsonData	)
+        let json = ["email_address": "test", "password": "test"]
+        let jsonData = try? JSONSerialization.data(withJSONObject: json, options: [])
+        //print(jsonData)
         
         /*var username = "test"
         var password = "test"
@@ -30,16 +30,23 @@ class APIGet {
         let finalBody = body.data(using: .utf8)*/
         
         request.httpMethod = "POST"
+        
+        
+        
         request.httpBody = jsonData
         
-        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
             
             if let data = data {
                 //let image = UIImage(data: data)
                 //print(response)
                 let jsonInfo = try? JSONSerialization.jsonObject(with: data, options: [])
                 if let jsonInfo = jsonInfo as? [String: Any] {
+                    print(response)
                     print(jsonInfo)
+                    
                 }
             } else if let error = error {
                 print("Error \(error)")
